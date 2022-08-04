4cc96a28742566003d2e54779cf24c223f381c74
Ivan_Marahovskiy <marahovskii81@gmail.com>
2022-08-04 09:54:33 +0500

    calc_base-front_v-01

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+$(function() {
+	$( ".btn" ).click(function(){
+		var valueBtn = $(this).text();
+		var result = $('#result').text(valueBtn);
+
+	})
+
+})
---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+let preprocessor = 'sass';
+
+const { src, dest, parallel, series, watch } = require('gulp');
+
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const sass = require('gulp-sass')(require('sass'));
+const cleancss = require('gulp-clean-css');
+
+function browsersync() {
+	browserSync.init({ // Инициализация Browsersync
+		server: { baseDir: 'app/' }, // Указываем папку сервера
+		notify: false, // Отключаем уведомления
+		online: true // Режим работы: true или false
+	})
+}
+
+function scripts() {
+	return src([ // Берем файлы из источников
+		'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
+		'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+		])
+	.pipe(concat('app.min.js')) // Конкатенируем в один файл
+	.pipe(uglify()) // Сжимаем JavaScript
+	.pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
+	.pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+}
+
+function styles() {
+	return src('app/' + preprocessor + '/main.' + preprocessor + '') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
+	.pipe(eval(preprocessor)()) // Преобразуем значение переменной "preprocessor" в функцию
+	.pipe(concat('app.min.css')) // Конкатенируем в файл app.min.js // Создадим префиксы с помощью Autoprefixer
+	.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } )) // Минифицируем стили
+	.pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
+	.pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+}
+
+function startwatch() {
+ 
+	// Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
+	watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
+	watch('app/**/' + preprocessor + '/**/*', styles);
+	watch('app/**/*.html').on('change', browserSync.reload);
+ 
+}
+
+
+exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.styles = styles;
+
+exports.default = parallel(styles, scripts, browsersync, startwatch);
